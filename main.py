0ed57823fce0d7a24349316a8fe45fd97a4e1ef2
mohammad <mr.dori.dev@gmail.com>
2021-11-28 21:08:53 +0330

    cleaning code

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-"""We have a challenge to do
-print all way answer this expression is 100
+"""We have a challenge to solve
+print all way calculate the expression is 100
 operator[o] = ["+" or "-" or " "]
 "+" means total
 "-" means submission
 " " means nothings(1 " " 2 means 12)
-1 o 2 o 3 o 4 o 5 o 6 o 7 o 8 o 9 = 100 
+1 o 2 o 3 o 4 o 5 o 6 o 7 o 8 o 9 = 100
 """
 
 from itertools import product
@@ -18,11 +18,11 @@ print("---------------- Smart-Solution ----------------")
 # We can use itertools.product(iter, repeat=int)
 
 for j1, j2, j3, j4, j5, j6, j7, j8 in product(OPERATOR, repeat=8):
-    value = f"1{j1}2{j2}3{j3}4{j4}5{j5}6{j6}7{j7}8{j8}9"
+    expression = f"1{j1}2{j2}3{j3}4{j4}5{j5}6{j6}7{j7}8{j8}9"
     # this replace for --> "1 2 + 4" => "12 + 4"
-    value = value.replace(" ", "")
-    if eval(value) == 100:
-        print(f"{value} == 100")
+    expression = expression.replace(" ", "")
+    if eval(expression) == 100:
+        print(f"{expression} == 100")
 
 
 print("---------------- Interesting-Solution ----------------")
@@ -38,11 +38,11 @@ for j1, j2 in two_nested_loop:
     for j3, j4 in two_nested_loop:
         for j5, j6 in two_nested_loop:
             for j7, j8 in two_nested_loop:
-                value = f"1{j1}2{j2}3{j3}4{j4}5{j5}6{j6}7{j7}8{j8}9"
+                expression = f"1{j1}2{j2}3{j3}4{j4}5{j5}6{j6}7{j7}8{j8}9"
                 # this replace for --> "1 2 + 4" => "12 + 4"
-                value = value.replace(" ", "")
-                if eval(value) == 100:
-                    print(f"{value} == 100")
+                expression = expression.replace(" ", "")
+                if eval(expression) == 100:
+                    print(f"{expression} == 100")
 
 
 print("---------------- Bad-Solution ----------------")
@@ -58,8 +58,8 @@ for j1 in OPERATOR:
                     for j6 in OPERATOR:
                         for j7 in OPERATOR:
                             for j8 in OPERATOR:
-                                value = f"1{j1}2{j2}3{j3}4{j4}5{j5}6{j6}7{j7}8{j8}9"
+                                expression = f"1{j1}2{j2}3{j3}4{j4}5{j5}6{j6}7{j7}8{j8}9"
                                 # this replace for --> "1 2 + 4" => "12 + 4"
-                                value = value.replace(" ", "")
-                                if eval(value) == 100:
-                                    print(f"{value} == 100")
+                                expression = expression.replace(" ", "")
+                                if eval(expression) == 100:
+                                    print(f"{expression} == 100")
