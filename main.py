d3987d123677fb39837d3bc2f1d12be2f8f74b5a
mohammad <mr.dori.dev@gmail.com>
2021-11-28 20:19:19 +0330

    add module doctstring

---
+++ main.py
@@ -0,0 +1,8 @@
+"""We have a challenge to do
+print all way answer this expression is 100
+operator[o] = ["+" or "-" or " "]
+"+" means total
+"-" means submission
+" " means nothings(1 " " 2 means 12)
+1 o 2 o 3 o 4 o 5 o 6 o 7 o 8 o 9 = 100 
+"""
