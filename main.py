6a0bb74c9ac29336c4f54d439038a438a67ede12
mohammad <mr.dori.dev@gmail.com>
2021-11-28 21:09:54 +0330

    change module docstring

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 """We have a challenge to solve
 print all way calculate the expression is 100
 operator[o] = ["+" or "-" or " "]
-"+" means total
-"-" means submission
-" " means nothings(1 " " 2 means 12)
+"+" means total("1+2" = "3")
+"-" means submission("1-2" = "-1")
+" " means nothings("1 2" = 12)
 1 o 2 o 3 o 4 o 5 o 6 o 7 o 8 o 9 = 100
 """
 
